ee502b4a78697728d7b85537cf67115ef87eef8c
Noah-Moring <148168395+Noah-Moring@users.noreply.github.com>
2024-02-06 17:57:16 -0500

    Tree Done(?)

---
+++ Tree.java
@@ -0,0 +1,11 @@
+public class Tree{
+    public int idNumber;
+    public int age;
+    public String species;
+
+    public Tree(int idNumber, int age, String species){
+        this.idNumber = idNumber;
+        this.age = age;
+        this.species = species;
+    }
+}
